88d68c1b3e03e4e8ef2ee68a5bee5340d9284085
Shreeyash Kailas Borse <68642592+shreeyash-hello@users.noreply.github.com>
2021-03-08 15:36:52 +0530

    Create functions.cpp

---
+++ functions.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int max_of_four(int a,int b,int c,int d){
+    int max;
+    if (a>b && a>c && a>d){
+        max=a;
+    }
+    else if (b>a && b>c && b>d){
+        max=b;
+    }
+    else if (c>b && c>a && c>d){
+        max=c;
+    }
+    else if (d>b && d>c && d>a){
+        max=d;
+    }
+    return max;
+}
+
+int main() {
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    int ans = max_of_four(a, b, c, d);
+    cout << ans;
+    
+    return 0;
+}
